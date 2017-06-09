46e90f2d460588fcc76624040dc583758df64f3a
kartikeya-b <bahuguna.kartikeya@gmail.com>
2017-06-09 15:35:38 +0530

    Create build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -eo pipefail
 
 # Install merge plugin.
-docker run --rm \
+sudo docker run --rm \
     -v "$HOME/.composer":/composer \
     -v "$PWD":/app \
     composer/composer:alpine update -n
 
 # Install all requirements.
-docker run --rm \
+sudo docker run --rm \
     -v "$HOME/.composer":/composer \
     -v "$PWD":/app \
     composer/composer:alpine update -n
