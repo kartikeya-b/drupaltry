ff4d4cbed6176e5fb554de16030bfa430995bfc8
kartikeya-b <bahuguna.kartikeya@gmail.com>
2017-06-09 15:36:33 +0530

    Create tests.sh

--- tests.sh
+++ tests.sh
@@ -2,13 +2,13 @@
 
 sudo chown -R 82:82 .
 
-docker-compose up -d mariadb
-docker-compose up -d nginx
+sudo docker-compose up -d mariadb
+sudo docker-compose up -d nginx
 
-docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
-docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
-docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
+sudo docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/Tests/Core/Password/PasswordHashingTest.php
+sudo docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/KernelTests/Component/Utility/SafeMarkupKernelTest.php
+sudo docker-compose run --user 82 php vendor/bin/phpunit -c core core/tests/Drupal/FunctionalTests/Breadcrumb/Breadcrumb404Test.php
 
 sudo chown -R jenkins:jenkins .
 
-docker-compose down --remove-orphans
+sudo docker-compose down --remove-orphans
